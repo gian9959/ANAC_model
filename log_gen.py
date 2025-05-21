e115a39d88072fe503f2f073a6a13469f0f0372d
Gianmaria Vasino <gianmaria.vasino@edu.unito.it>
2025-05-21 11:34:14 +0200

    config file

---
+++ log_gen.py
@@ -0,0 +1,33 @@
+import os
+import json
+import pandas
+import torch
+from torch.utils.data import DataLoader
+
+from anac_model.anac_dataset import AnacDataset
+from anac_model.collate_normalization import collate_fn
+from anac_model.learning_functions import validation
+
+print('Loading validation dataset...')
+val_dataset = AnacDataset('./data/anac_validation.csv')
+val_loader = DataLoader(val_dataset, batch_size=1, collate_fn=collate_fn, shuffle=False)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+epochs = []
+tr_loss = []
+val_loss = []
+
+for check_path in os.listdir(config["log_source"]):
+    path = config["log_source"] + '/' + check_path
+
+    checkpoint = torch.load(path)
+    epochs.append(checkpoint['epoch'])
+    tr_loss.append(checkpoint['tr_loss'])
+
+    val_loss.append(validation(val_loader, checkpoint_path=path))
+
+csv_dict = {'EPOCH': epochs, 'TR_LOSS': tr_loss, 'VAL_LOSS': val_loss}
+csv_df = pandas.DataFrame(csv_dict)
+csv_df.to_csv(config['log_dest'])
--- routine.py
+++ routine.py
@@ -1,11 +1,10 @@
+import json
 from torch.utils.data import DataLoader
 
 from anac_model.anac_dataset import AnacDataset
 from anac_model.collate_normalization import collate_fn
 from anac_model.learning_functions import training
 from anac_model.learning_functions import validation
-
-checkpoint_path = ''
 
 print('Loading training dataset...')
 tr_dataset = AnacDataset('./data/anac_training.csv')
@@ -15,7 +14,12 @@ print('Loading validation dataset...')
 val_dataset = AnacDataset('./data/anac_validation.csv')
 val_loader = DataLoader(val_dataset, batch_size=1, collate_fn=collate_fn, shuffle=False)
 
-for i in range(10):
-    checkpoint_path = training(tr_loader, checkpoint_path=checkpoint_path)
-    val_loss = validation(val_loader, checkpoint_path=checkpoint_path)
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
+checkpoint_path = config['checkpoint']
+hidden_layers = config['hidden_layers']
+
+for i in range(10):
+    checkpoint_path = training(tr_loader, checkpoint_path=checkpoint_path, hl=hidden_layers)
+    val_loss = validation(val_loader, checkpoint_path=checkpoint_path)
